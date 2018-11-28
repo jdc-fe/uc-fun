8f5d5bfee2551ce5aa267949ac3dd34d43defe85
史心甜 <shixintian@jd.com>
2018-11-28 17:35:08 +0800

    修改promise

--- test/unit/delayPromise.spec.js
+++ test/unit/delayPromise.spec.js
@@ -3,13 +3,12 @@ import delayPromise from '../../src/delayPromise';
 let preVal = 0;
 
 describe('delayPromise', () => {
-  it('normal: should run function after delay time', async () => {
-    await assert.doesNotReject(
-      delayPromise((...args) => {
-        [preVal] = [...args];
-      }, 3000, 100).then(() => {
-        assert(preVal === 100);
-      })
-    );
+  beforeEach(() => {
+    delayPromise((...args) => {
+      [preVal] = [...args];
+    }, 3000, 100);
+  });
+  it('normal: should run function after delay time', () => {
+    assert(preVal === 100);
   });
 });
