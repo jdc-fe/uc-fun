bb370bf1bddfde956ceb0fe6ec198ac874bcc281
史心甜 <shixintian@jd.com>
2018-11-29 19:48:36 +0800

    sleep resolve

--- test/unit/sleep.spec.js
+++ test/unit/sleep.spec.js
@@ -1,23 +1,13 @@
 import sleep from '../../src/sleep';
 
 describe('sleep', () => {
-  let clock;
-  before(() => { clock = sinon.useFakeTimers(); });
-  after(() => { clock.restore(); });
-
-  it('normal: should run function after delay time', () => {
-    const callFunc = async (func) => {
-      await sleep(100);
-      func();
-    };
-    const callback = sinon.fake();
-    callFunc(callback);
-
-    clock.tick(99);
-    assert(callback.notCalled);
-
-    clock.tick(1);
-    assert.strictEqual(new Date().getTime(), 100);
-    assert(callback.calledOnce);
+  it('normal: should run function after delay time', (done) => {
+    const clock = sinon.useFakeTimers();
+    sleep(100).then(() => {
+      assert.strictEqual(new Date().getTime(), 100);
+      clock.restore();
+      done();
+    });
+    clock.tick(100);
   });
 });
