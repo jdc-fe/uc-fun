95b907bf593f3baaf64aef9018e6c15d9b16139a
史心甜 <shixintian@jd.com>
2018-12-05 12:34:30 +0800

    isBigger限制参数类型

--- test/unit/isBigger.spec.js
+++ test/unit/isBigger.spec.js
@@ -6,23 +6,10 @@ describe('isBigger', () => {
     assert(exa1);
   });
 
-  it('normal: param can be converted into a number', () => {
-    const exa1 = isBigger('  10  ', '3');
-    const exa2 = isBigger('  10  ', new Date());
-    const exa3 = isBigger(null, 10);
-    assert(exa1);
-    assert(!exa2);
-    assert(!exa3);
-  });
-
-  it('error: param canot be converted into a number', () => {
-    const exa1 = () => {
-      isBigger(() => { }, 3);
+  it('error: param is not Number type', () => {
+    const result = () => {
+      isBigger('10', '2');
     };
-    const exa2 = () => {
-      isBigger(3, undefined);
-    };
-    assert.throws(exa1, Error);
-    assert.throws(exa2, Error);
+    assert.throws(result, Error);
   });
 });
--- test/unit/stringEllipsis.spec.js
+++ test/unit/stringEllipsis.spec.js
@@ -3,17 +3,23 @@ import stringEllipsis from '../../src/stringEllipsis';
 describe('stringEllipsis', () => {
   it('normal', () => {
     const exa1 = stringEllipsis('aaabbbccc', 3);
-    const exa2 = stringEllipsis('aaabbbccc', '3');
-    const exa3 = stringEllipsis('aaa', '10');
+    const exa2 = stringEllipsis('aa', 3);
     assert(exa1 === 'aaa...');
-    assert(exa2 === 'aaa...');
-    assert(exa3 === 'aaa');
+    assert(exa2 === 'aa');
   });
 
   it('error: param is not string type', () => {
-    const exa1 = stringEllipsis(() => { }, 3);
-    const exa2 = stringEllipsis(323, 1);
-    assert(exa1 === '');
-    assert(exa2 === '');
+    const exa1 = () => {
+      stringEllipsis(123, 2);
+    };
+    const exa2 = () => {
+      stringEllipsis('aaabbbccc', '3');
+    };
+    const exa3 = () => {
+      stringEllipsis('aaa', '10');
+    };
+    assert.throws(exa1, Error);
+    assert.throws(exa2, Error);
+    assert.throws(exa3, Error);
   });
 });
