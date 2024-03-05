f43747c64b1218702c608a421870646a3c9e7930
houlinjiang <houlinjiang@jd.com>
2024-03-05 16:48:13 +0800

    add run promise list by queue

---
+++ test/promiseQueue.spec.ts
@@ -0,0 +1,15 @@
+import assert = require('power-assert');
+import promiseQueue from '../src/promiseQueue';
+import { sleep } from '../src';
+
+describe.only('promiseQueue', () => {
+  it('normal', async () => {
+    console.log('start', new Date().toISOString());
+    await promiseQueue([1,2,3].map((i) => {
+      return async () => {
+        console.log('print', i, new Date().toISOString());
+        await sleep(100);
+      }
+    }));
+  });
+})
