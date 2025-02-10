de8b6ec45095b3dd04c60b290fa061c32d8123fd
houlinjiang <houlinjiang@jd.com>
2025-02-10 14:37:39 +0800

    fix randomStr unit test

--- test/randomStr.spec.ts
+++ test/randomStr.spec.ts
@@ -6,8 +6,8 @@ describe('randomStr', () => {
     const str1 = randomStr(1)
     assert.ok(str1.length === 1);
 
-    assert.ok(randomStr(0).length === 11);
-    assert.ok(randomStr(20).length === 11);
+    assert(randomStr(0).length === 11);
+    assert(randomStr(20).length === 11);
 
     const prefix = 'pre';
     assert.ok(randomStr(2, prefix).length === prefix.length + 3);
